7cb07d9d8a2501b4e286a940f50b817379ba1b5b
Falcon <guiedsouza@gmail.com>
2021-09-18 00:01:33 -0300

    Update convertLink.js

--- convertLink.js
+++ convertLink.js
@@ -1,12 +1,3 @@
-function toHyperlink()
-{
-    var str = document.getElementById("urltext").value;
-    
-    var pattern1 = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    var str1 = str.replace(pattern1, "<a href='$1'>$1</a>");
-    
-    var pattern2 =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    var str2 = str1.replace(pattern2, '$1<a target="_blank" href="http://$2">$2</a>');
-    
-    document.getElementById("result").innerHTML = str2;
+function toHyperlink() {
+  alert("LINK");
 }
